e07be657f7f5c1d5127802f6e9052164a9af2f03
Naveen <none.wiseguy@gmail.com>
2016-01-13 13:31:35 +0530

    Added search feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,17 @@
 class ArticlesController < ApplicationController
   def index
     if Rails.env.production?
-    @articles=Article.order("views DESC","published DESC NULLS LAST").page(params[:page])
+    if params[:search]
+      @articles=Article.title_has(params[:search]).order("views DESC","published DESC NULLS LAST").page(params[:page])
+    else
+      @articles=Article.order("views DESC","published DESC NULLS LAST").page(params[:page])
+    end
   else
-    @articles=Article.order("views DESC","published DESC").page(params[:page])
+    if params[:search]
+      @articles=Article.title_has(params[:search]).order("views DESC","published DESC").page(params[:page])
+    else
+      @articles=Article.order("views DESC","published DESC").page(params[:page])
+    end
   end
   end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
   belongs_to :blog
+
+  scope :title_has, ->(key) {where("title LIKE ?","%#{key}%")}
 end
